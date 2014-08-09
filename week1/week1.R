d9b43307684274b6a706ad2eed892234e323fc67
Hojoon Ji <sleeperbus@gmail.com>
2014-08-09 21:30:31 +0900

    ...

--- week1/week1.R
+++ week1/week1.R
@@ -1,3 +1,11 @@
+library(kernlab)
+data(spam)
+set.seed(3435)
+trainIndicator <- rbinom(nrow(spam), size = 1, prob = 0.5)
+trainSpam <- spam[trainIndicator == 1, ]
+testSpam <- spam[trainIndicator == 0, ]
+with(trainSpam, table(type))
+
 trainSpam$numType <- as.numeric(trainSpam$type) - 1
 costFunction <- function(x, y) sum(x != (y > 0.5))
 cvError = rep(NA, 55)
@@ -7,3 +15,8 @@ for (i in 1:55) {
     glmFit = glm(lmFormula, family = "binomial", data = trainSpam)
     cvError[i] = cv.glm(trainSpam, glmFit, costFunction, 2)$delta[2]
 }
+
+names(trainSpam)[which.min(cvError)]
+
+predictionModel <- glm(numType ~ charDollar,  family="binomial", data=trainSpam)
+predictionTest <- predict(predictionModel, testSpam)
